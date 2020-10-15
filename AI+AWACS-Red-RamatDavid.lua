1020e8441f02511dacb808011d6b273822a2d777
chandawg <johnathan.chandler@gmail.com>
2020-10-15 02:35:24 -0400

    changed the so that callsigns are retained

--- AI+AWACS-Red-RamatDavid.lua
+++ AI+AWACS-Red-RamatDavid.lua
@@ -13,22 +13,18 @@ RamatDavid_SouthCAPZone = ZONE_POLYGON:New( "Ramat David CAP Zone", GROUP:FindBy
 TEXACOTankerRed2 = SPAWN
   :NewWithAlias("B_TANKER_KC135MPRS_4","TEXACO Red 2")
   :InitLimit(1,3)
-  :InitCleanUp(30)
-  :OnSpawnGroup(function(SpawnedGroup)
-  MESSAGE:New("TEXACO Red 2-1 is back on station",25,"TEXACO Red 2-1"):ToRed()
-  end)
+  --:InitCleanUp(30)
+  :OnSpawnGroup(function(SpawnedGroup)MESSAGE:New("TEXACO Red 2-1 is back on station",25,"TEXACO Red 2-1"):ToRed()end)
   :SpawnScheduled(3600,0.1) 
 
-TEXACOTankerRed2 = SPAWN
+SHELLTankerRed2 = SPAWN
   :NewWithAlias("B_TANKER_KC135_4","SHELL Red 2")
   :InitLimit(1,3)
-  :InitCleanUp(30)
-  :OnSpawnGroup(function(SpawnedGroup)
-  MESSAGE:New("SHELL Red 2-1 is back on station",25,"SHELL Red 2-1"):ToRed()
-  end)
+  --:InitCleanUp(30)
+  :OnSpawnGroup(function(SpawnedGroup)MESSAGE:New("SHELL Red 2-1 is back on station",25,"SHELL Red 2-1"):ToRed()end)
   :SpawnScheduled(3600,0.1) 
 
-RedA2ADispatcher2:SetSquadron( "23rd Fighter Aviation Regiment", AIRBASE.Syria.Ramat_David, { "23rd Fighter Aviation Regiment" }, 12 ) 
+RedA2ADispatcher2:SetSquadron( "23rd Fighter Aviation Regiment", AIRBASE.Syria.Ramat_David, { "23rd Fighter Aviation Regiment" }, 12 )
 --MiG-31 Squadron
 RedA2ADispatcher2:SetSquadronCap( "23rd Fighter Aviation Regiment", RamatDavid_SouthCAPZone, 1000, 12000, 600, 800, 800, 1200, "BARO" )
 RedA2ADispatcher2:SetSquadronCapInterval( "23rd Fighter Aviation Regiment", 2, 180, 540, 1 )
