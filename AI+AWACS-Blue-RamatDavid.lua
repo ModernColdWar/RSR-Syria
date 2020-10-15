66b4db843322f9091ce2a7036bb79e3aa2882920
chandawg <johnathan.chandler@gmail.com>
2020-10-15 02:34:11 -0400

    typo
    made a change so that the tankers would keep their call signs, and made a typo in the ramat david blue awacs

--- AI+AWACS-Blue-RamatDavid.lua
+++ AI+AWACS-Blue-RamatDavid.lua
@@ -13,19 +13,15 @@ RamatDavid_SouthCAPZone = ZONE_POLYGON:New( "Ramat David CAP Zone", GROUP:FindBy
 TEXACOTankerBlue2 = SPAWN
   :NewWithAlias("B_TANKER_KC135MPRS_3","TEXACO Blue 2")
   :InitLimit(1,3)
-  :InitCleanUp(30)
-  :OnSpawnGroup(function(SpawnedGroup)
-  MESSAGE:New("TEXACO Blue 2-1 is back on station",25,"TEXACO Blue 2-1"):ToBlue()
-  end)
+--  :InitCleanUp(30)
+  :OnSpawnGroup(function(SpawnedGroup)MESSAGE:New("TEXACO Blue 2-1 is back on station",25,"TEXACO Blue 2-1"):ToBlue()end)
   :SpawnScheduled(3600,0.1) 
 
-TEXACOTankerBlue2 = SPAWN
+SHELLTankerBlue2 = SPAWN
   :NewWithAlias("B_TANKER_KC135_3","SHELL Blue 2")
   :InitLimit(1,3)
   :InitCleanUp(30)
-  :OnSpawnGroup(function(SpawnedGroup)
-  MESSAGE:New("SHELL Blue 2-1 is back on station",25,"SHELL Blue 2-1"):ToBlue()
-  end)
+  :OnSpawnGroup(function(SpawnedGroup)MESSAGE:New("SHELL Blue 2-1 is back on station",25,"SHELL Blue 2-1"):ToBlue()end)
   :SpawnScheduled(3600,0.1) 
 
 BlueA2ADispatcher2:SetSquadron( "314th Tactical Fighter Squadron", AIRBASE.Syria.Ramat_David, { "314th Tactical Fighter Squadron" }, 12 ) --F4 Squadron
