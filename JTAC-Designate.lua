635ca9d233a0c321f574a4ff3fb99c4e1a3cacc1
chandawg <johnathan.chandler@gmail.com>
2020-11-02 12:21:55 -0500

    modified to work with UAVs

--- JTAC-Designate.lua
+++ JTAC-Designate.lua
@@ -1,36 +1,34 @@
-BlueRecceSetGroup = SET_GROUP:New():FilterCoalitions("blue"):FilterPrefixes( {"JTAC Test ", "_M1128 Stryker MGS_"} ):FilterStart()
-
-RedRecceSetGroup = SET_GROUP:New():FilterCoalitions("red"):FilterPrefixes( {"JTAC Test ", "_BMP-2_"} ):FilterStart()
-
 BlueHQ = GROUP:FindByName( "Blue HQ" )
 RedHQ = GROUP:FindByName( "Red HQ" )
+
 BlueCommandCenter = COMMANDCENTER
   :New( BlueHQ, "Allied Command" )
 RedCommandCenter = COMMANDCENTER
   :New( RedHQ, "Allied Command" )
 
---HQ = GROUP:FindByName("Blue HQ")
-
---CC = COMMANDCENTER:New( HQ, "JTAC" )
---CC = COMMANDCENTER:New( HQ, "Leopard 1A3 (Recon)" )
+BlueRecceSetGroup = SET_GROUP:New():FilterCoalitions("blue"):FilterPrefixes( {"JTAC Test ", "PONTIAC 5", "PONTIAC 6"} ):FilterStart()
+RedRecceSetGroup = SET_GROUP:New():FilterCoalitions("red"):FilterPrefixes( {"JTAC Test ", "PONTIAC 7", "PONTIAC 8"} ):FilterStart()
 
 BLUE_CAS_Set = SET_GROUP:New():FilterPrefixes( {" Blue AF", " Blue Cargo"} ):FilterStart()
 RED_CAS_Set = SET_GROUP:New():FilterPrefixes( {" Red AF", " Red Cargo"} ):FilterStart()
 
-BlueRecceDetection = DETECTION_AREAS:New(BlueRecceSetGroup, 10000)
-  :SetAcceptRange(10000)
-  :FilterCategories( { Unit.Category.GROUND_UNIT, Unit.Category.STRUCTURE } )
-  :SetAlphaAngleProbability(0.9)
+BlueRecceDetection = DETECTION_AREAS:New(BlueRecceSetGroup, 25000)
+  :SetAcceptRange(25000)
+  :FilterCategories( { Unit.Category.GROUND_UNIT } )
+--  :SetAlphaAngleProbability(0.9)
+--  :SetDistanceProbability(0.9)
 
-RedRecceDetection = DETECTION_AREAS:New(RedRecceSetGroup, 10000)
-  :SetAcceptRange(10000)
-  :FilterCategories( { Unit.Category.GROUND_UNIT, Unit.Category.STRUCTURE } )
-  :SetAlphaAngleProbability(0.9)
+RedRecceDetection = DETECTION_AREAS:New(RedRecceSetGroup, 25000)
+  :SetAcceptRange(25000)
+  :FilterCategories( { Unit.Category.GROUND_UNIT } )
+--  :SetAlphaAngleProbability(0.9)
+--  :SetDistanceProbability(0.9)
 
 BlueReconDesignation = DESIGNATE:New( BlueCommandCenter, BlueRecceDetection, BLUE_CAS_Set)
   :SetThreatLevelPrioritization(true)
-  :SetMaximumDistanceAirDesignation(10000)
+  :SetMaximumDistanceAirDesignation(25000)
   :SetMaximumDistanceDesignations(25000)
+  :SetMaximumDistanceGroundDesignation(25000)
   :SetMaximumDesignations(4)
   :SetLaserCodes({1682, 1683, 1684, 1685})
   :SetDesignateName("JTAC")
@@ -42,8 +40,9 @@ BlueReconDesignation = DESIGNATE:New( BlueCommandCenter, BlueRecceDetection, BLU
 
 RedReconDesignation = DESIGNATE:New( RedCommandCenter, RedRecceDetection, RED_CAS_Set)
   :SetThreatLevelPrioritization(true)
-  :SetMaximumDistanceAirDesignation(10000)
+  :SetMaximumDistanceAirDesignation(25000)
   :SetMaximumDistanceDesignations(25000)
+  :SetMaximumDistanceGroundDesignation(25000)  
   :SetMaximumDesignations(4)
   :SetLaserCodes({1686, 1687, 1688, 1689 })
   :SetDesignateName("JTAC")
